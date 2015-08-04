8f2a07a7c5012c895b468ff985efbd36238de8df
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-08-03 18:57:26 -0700

    manufacture to brand

--- createSk8Tables.sql
+++ createSk8Tables.sql
@@ -12,16 +12,42 @@ DROP TABLE IF EXISTS '';
 CREATE TABLE sk8_riders (
   id INT NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
-  name varchar(255) NOT NULL, DEFAULT 
+  img_url varchar(255) NOT NULL, DEFAULT 'http://web.engr.oregonstate.edu/~swansonb/dataFinal/profileshadow.jpg',
   PRIMARY KEY (id)
-)
+);
 
-CREATE TABLE sk8_riders (
+CREATE TABLE sk8_brand (
   id INT NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
-
+  img_url varchar(255),
   PRIMARY KEY (id)
-)
+);
+
+CREATE TABLE sk8_wheel_type(
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  fk_brand_id INT NOT NULL,
+  diamater INT NOT NULL,
+  durometer INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_brand_id) REFERENCES sk8_brand (id)
+);
+
+CREATE TABLE sk8_truck_type(
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  fk_brand_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_brand_id) REFERENCES sk8_brand (id)
+);
+
+CREATE TABLE sk8_deck_type(
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  fk_brand_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (fk_brand_id) REFERENCES sk8_brand (id)
+);
 
 ---------------------------------------
 ----Populate Tables--------------------
