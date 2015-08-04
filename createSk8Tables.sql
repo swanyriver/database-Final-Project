55b1370c2bcdb5dd5cc17255d913ccc679d2d075
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-08-03 18:01:20 -0700

    profile picture and begining of create sql

---
+++ createSk8Tables.sql
@@ -0,0 +1,28 @@
+---------------------------------------
+--ensure starting brand new tables ----
+---------------------------------------
+DROP TABLE IF EXISTS '';
+
+
+
+---------------------------------------
+-----Create sk8 Tables-----------------
+---------------------------------------
+
+CREATE TABLE sk8_riders (
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+  name varchar(255) NOT NULL, DEFAULT 
+  PRIMARY KEY (id)
+)
+
+CREATE TABLE sk8_riders (
+  id INT NOT NULL AUTO_INCREMENT,
+  name varchar(255) NOT NULL,
+
+  PRIMARY KEY (id)
+)
+
+---------------------------------------
+----Populate Tables--------------------
+---------------------------------------
