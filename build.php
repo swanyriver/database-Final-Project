dbeabee77897f98a6f0660d0461d0bc3d9152863
swanyriver <swanyriver@gmail.com>
2015-08-07 18:07:28 -0700

    filtering set up

--- build.php
+++ build.php
@@ -15,41 +15,107 @@ if ($mysqli->connect_errno || $mysqli->connect_error)
   exit();
 }
 
+if(isset($_POST['available'])){
+  //only show parts not on boards
+  $deckQuery ="";
+  $truckQuery="";
+  $wheelQuery="";
+}else {
+  $deckQuery =  
+  "SELECT D.id, D.color as deckColor, DT.deck_name, DT.length, DT.description, B.brand_name as deck_brand_name, B.brand_img_url as deck_brand_img_url, D.fk_deck_id as fkid FROM sk8_deck_inv D
+  INNER JOIN sk8_deck_type DT on D.fk_deck_id = DT.id 
+  INNER JOIN sk8_brand B on DT.fk_brand_id = B.id";
+  
+  $truckQuery=
+  "SELECT T.id, TT.truck_name, TT.width, B.brand_name as truck_brand_name, B.brand_img_url as truck_brand_img_url, T.fk_truck_id as fkid FROM sk8_truck_inv T
+  INNER JOIN sk8_truck_type TT on T.fk_truck_id = TT.id 
+  INNER JOIN sk8_brand B on TT.fk_brand_id = B.id";
+
+  $wheelQuery=
+  "SELECT W.id, W.color as wheelColor, WT.wheel_name, WT.diameter, WT.durometer, B.brand_name as wheel_brand_name, B.brand_img_url as wheel_brand_img_url, W.fk_wheel_id as fkid FROM sk8_wheel_inv W
+  INNER JOIN sk8_wheel_type WT on W.fk_wheel_id=WT.id
+  INNER JOIN sk8_brand B on WT.fk_brand_id = B.id";
+}
+
 
 include "headandnav.php";
 echo "<script> document.getElementById('build_tab').classList.add('active'); </script>";
 
  ?>
 
-<script type="text/javascript">
-  
-  function buildselect(panel){
-    console.log('clicked')
-    console.log(panel.getAttribute('data-id'),panel);
-  }
+ <div class="container-fluid">
+  <div class="row">
+    <div class="col-lg-2"></div>
+    <div class="col-lg-8" id="boardTemplate">
+      
+      <div class="container-fluid">
+        <div class="row">
+        <div class="col-lg-4">
+          <span class="builpartlabel">Deck</span>
+          <div class="buildPartHolder" id="deckPart"></div>
+        </div>
+        <div class="col-lg-4">
+          <span class="builpartlabel">Truck</span>
+          <div class="buildPartHolder" id="truckPart"></div>
+        </div>
+        <div class="col-lg-4">
+          <span class="builpartlabel">Wheel</span>
+          <div class="buildPartHolder" id="wheelPart"></div>
+        </div>
+      </div></div>
 
-  function hello(msg){
-    console.log(msg);
+
+    </div>
+    <div class="col-lg-2"></div>
+  </div>
+ </div>
+
+<script type="text/javascript">
+
+  var deckPart;
+  var deckkey;
+  var truckPart;
+  var deckkey;
+  var wheelPart;
+  var wheelkey;
+  
+  function buildselect(panel,partpanel,formfield){
+    console.log(panel.getAttribute('data-id'),panel);
+
+    partpanel.innerHTML='';
+    partpanel.appendChild(panel.cloneNode(true));
+
+    formfield.setAttribute('value',panel.getAttribute('data-id'));
+
+    if(deckkey.value && truckkey.value && wheelkey.value){
+      console.log('ready');
+    }
+
   }
 
   window.onload=function setonclicks () {
 
-    console.log('hello');
+    deckPart=document.getElementById('deckPart');
+    truckPart=document.getElementById('truckPart');
+    wheelPart=document.getElementById('wheelPart');
+    deckkey=document.getElementById('deckkey');
+    truckkey=document.getElementById('truckkey');
+    wheelkey=document.getElementById('wheelkey');
 
     decks=document.getElementsByClassName('deckbuild');
     for (var i = decks.length - 1; i >= 0; i--) {
       //decks[i].setAttribute("onclick","click(this,'deck')");
-      decks[i].setAttribute("onclick","buildselect(this)");
+      decks[i].setAttribute("onclick","buildselect(this,deckPart,deckkey)");
     };
 
     trucks=document.getElementsByClassName('truckbuild');
     for (var i = trucks.length - 1; i >= 0; i--) {
-      trucks[i].setAttribute("onclick","buildselect(this)");
+      trucks[i].setAttribute("onclick","buildselect(this,truckPart,truckkey)");
     };
 
     wheels=document.getElementsByClassName('wheelbuild');
     for (var i = wheels.length - 1; i >= 0; i--) {
-      wheels[i].setAttribute("onclick","buildselect(this)");
+      wheels[i].setAttribute("onclick","buildselect(this,wheelPart,wheelkey)");
     };
   }
 
@@ -67,11 +133,7 @@ echo "<script> document.getElementById('build_tab').classList.add('active'); </s
 
 <?php
 #generate decks
-$query =
-  "SELECT D.id, D.color as deckColor, DT.deck_name, DT.length, DT.description, B.brand_name as deck_brand_name, B.brand_img_url as deck_brand_img_url, D.fk_deck_id as fkid FROM sk8_deck_inv D
-  INNER JOIN sk8_deck_type DT on D.fk_deck_id = DT.id 
-  INNER JOIN sk8_brand B on DT.fk_brand_id = B.id";
-$result = $mysqli->query($query);
+$result = $mysqli->query($deckQuery);
 while($row=$result->fetch_assoc()){
   makeDeckInv($row, 'buildPanel deckbuild');
 }
@@ -84,11 +146,7 @@ while($row=$result->fetch_assoc()){
 
 <?php
 #generate trucks
-$query =
-  "SELECT T.id, TT.truck_name, TT.width, B.brand_name as truck_brand_name, B.brand_img_url as truck_brand_img_url, T.fk_truck_id as fkid FROM sk8_truck_inv T
-  INNER JOIN sk8_truck_type TT on T.fk_truck_id = TT.id 
-  INNER JOIN sk8_brand B on TT.fk_brand_id = B.id";
-$result = $mysqli->query($query);
+$result = $mysqli->query($truckQuery);
 while($row=$result->fetch_assoc()){
   makeTruckInv($row, 'buildPanel truckbuild');
 }
@@ -98,16 +156,11 @@ while($row=$result->fetch_assoc()){
   <div class="row" id="inventory_container">
     <div class="col-md-4">
       <h3 class="inventory_heading">Wheels</h3>
-<!--create new wheel-->
 
 
 <?php
 #generate wheels
-$query = 
-  "SELECT W.id, W.color as wheelColor, WT.wheel_name, WT.diameter, WT.durometer, B.brand_name as wheel_brand_name, B.brand_img_url as wheel_brand_img_url, W.fk_wheel_id as fkid FROM sk8_wheel_inv W
-  INNER JOIN sk8_wheel_type WT on W.fk_wheel_id=WT.id
-  INNER JOIN sk8_brand B on WT.fk_brand_id = B.id";
-$result = $mysqli->query($query);
+$result = $mysqli->query($wheelQuery);
 while($row=$result->fetch_assoc()){
   makeWheelInv($row, 'buildPanel wheelbuild');
 }
@@ -130,14 +183,18 @@ echo" </div>
     <div class="modal-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">What color is it?</h4>
+        <h4 class="modal-title">Your New Board is almost built</h4>
       </div>
       <div class="modal-body">
-      <form action="addinv.php" method="post">
-      <input id="colorid" type="hidden" name="id" value=""></input>
-      <input id="colortable" type="hidden" name="table" value=""></input>
-      <input id="colorfkid" type="hidden" name="fkid" value=""></input>
-      <input type="text" name="color"></input>
+      <form action="buildboardsql.php" method="post">
+
+      <input id="deckkey" type="hidden" name="fk_deck_id" value=""></input>
+      <input id="truckkey" type="hidden" name="fk_truck_id" value=""></input>
+      <input id="wheelkey" type="hidden" name="fk_wheel_id" value=""></input>
+      
+      What is it's name:<input type="text" name="board_name"></input><br>
+      (Optional) URL for a picture of it<input type="text" name="board_img_url"></input>
+
       <button type="submit">
         <span class="glyphicon glyphicon-plus"> </span>
         </button>
